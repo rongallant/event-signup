0e8be82ee627be3cf8a345e987628324bbedb467
Ron Gallant <ron@rongallant.com>
2016-02-11 20:46:51 +0000

    Person: Address now saving

--- routes/api/event.js
+++ routes/api/event.js
@@ -1,53 +1,59 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express'),
+  router = express.Router(),
+  mongoose = require('mongoose')
 
 var Event = require("../../models/event")
 
 var URL_BASE = "/admin/events"
 
-/* GET Returns all item. */
+/* LIST Returns all item. */
 router.get('/', function(req, res, next) {
   Event.find(function(err, data) {
-    if (!err) {
-      res.json(data)
-    }
-    else {
-      res.status(404).json({ error: err.message })
-    }
-  })
-})
-
-/* POST New item created. */
-router.post('/', function(req, res, next) {
-  var data = new Event({
-      name: req.body.name,
-      description: req.body.description
-  })
-  data.save(function(err) {
-      if (err) {
-        res.status(500).json({ error: err.message })
-      } else {
-        res.redirect(URL_BASE + '/success/created/' + data._id)
-      }
-  })
-})
-
-/* GET Returns single item. */
-router.get('/:id', function(req, res, next) {
-  Event.findById(req.params.id, function(err, data) {
     if (err) {
-        res.status(404).json({ error: err.message })
+        res.json({ error: err })
     } else {
       res.json(data)
     }
   })
 })
 
-/* PUT Updates an item. */
+/* CREATE New item created. */
+router.post('/', function(req, res, next) {
+  var data = new Event({
+    _address: { type: mongoose.Types.ObjectId, ref: 'Address' },
+    _contact: { type: mongoose.Types.ObjectId, ref: 'Person' },
+      name: req.body.name,
+      description: req.body.description
+  })
+
+    // schedules: [{
+    //     type: Schema.Types.ObjectId, ref: 'ScheduleDate'
+    // }],
+  data.save(function(err) {
+      if (err) {
+        res.json({ error: err })
+      } else {
+        res.redirect(URL_BASE + '/success/created/' + data._id)
+      }
+  })
+})
+
+/* EDIT Returns single item. */
+router.get('/:id', function(req, res, next) {
+  Event.findById(req.params.id, function(err, data) {
+    if (err) {
+        res.json({ error: err })
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+/* UPDATE Updates an item. */
 router.put('/:id', function(req, res, next) {
   Event.findById(req.params.id).update({$set:req.body}, function (err, data) {
       if (err) {
-        res.status(500).json({ error: err.message })
+        res.json({ error: err })
       } else {
         res.redirect(URL_BASE + '/success/updated/' + req.params.id)
       }
@@ -58,7 +64,7 @@ router.put('/:id', function(req, res, next) {
 router.delete('/:id', function(req, res, next) {
   Event.findOneAndRemove(req.params.id, function (err) {
       if (err) {
-        res.status(500).json({ error: err.message })
+        res.json({ error: err })
       } else {
         res.redirect(URL_BASE + '/success/deleted')
       }
